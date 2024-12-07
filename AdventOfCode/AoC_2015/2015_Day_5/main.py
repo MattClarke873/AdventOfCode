d725b70c582ae452bc152a60724218cc34e60490
MattClarke873 <matclarke216@hotmail.com>
2024-12-07 15:30:51 +0000

    complete Day 5

--- AdventOfCode/AoC_2015/2015_Day_5/main.py
+++ AdventOfCode/AoC_2015/2015_Day_5/main.py
@@ -46,47 +46,43 @@ def part2(data):
     """Function Solves problem two."""
     nice = 0
     word_list = data.splitlines()
-    print('start')
     for word in word_list:
         double_double = False
         repeat = False
 
-        for i in range(len(word) - 1):
-            double = None
-            if i > 0:
-                if word[i-1] == word[i]:
-                    double = word[i-1] + word[i]
-                    print(f'double {double}')
-                for x in range(len(word)-2):
-                    if x > 1:
-                        double_check = word[x-2] + word[x-1]
-                        
-                        if double == double_check:
-                            print({x-2}, {i-1})
-                            if ((x-2) - (i-1))  > 2:
-                                print('SNAP')
-                                double_double = True
-                                break
-
-                            '''NEED TO LOOK IN TO WHY THIS ISNT WORKING?? AAA CANT OVER'''
-
-     
-
-        for i in range(len(word) -1):
+#CHECKS IF DOUBLE DOUBLE, NOT ALLOWED TO OVERLAP
+        #print(word)
+        for i, char in enumerate(word):
             if i > 0:
                 sample = word[i-1] + word[i]
-                for x in range(len(word) -2):
-                    if x > 3:
-                        check = word[x-2] + word[x-1] 
+                #print()
+                #print(f'        {i-1} {i}')
+                #print(f'sample = {sample}')
+                
+                for x in range(len(word)+1):
+                    if x > 1:
+                        check = word[x-2] + word[x-1]
+                        #print(f'     _________________')
+                        #print(f'     {x-2} {x-1}')
+                        #print(f'     check = {check}')
+                        
                         if check == sample:
-                            if not (i-1) == (x-2) and not (i) == (x-1):
-                                
-                               # print(f'sample letters {sample} check letter {check}')
+                            if not (i-1 == x-2 and i == x-1) and (i-1 >= x-2 + 2 or  i-1 <= x-2 - 2):
+                                #print(f'     sample letters {sample} MATCHS check letter {check}')
                                # print(f'sample index {i-1}, {i} check letter index {x-2}, {x-1}')
-                                repeat = True
-                           
+                                double_double = True
+#CHECKS IF REPEATS WITH EXACTLY 1 SPACE        for i in range(len(word)):
+        for i, char in enumerate(word):
+            if i > 1:
+                sample = word[i]
+                if sample == word[i-2]:
+                    
+                    repeat = True
 
-        if double_double and repeat:
+
+
+        if  repeat and double_double:
+            
             nice +=1
 
 
@@ -94,4 +90,6 @@ def part2(data):
 
 
 
-part2(test_data) 
+part2(data) 
+
+
--- AdventOfCode/AoC_2015/2015_Day_6/main.py
+++ AdventOfCode/AoC_2015/2015_Day_6/main.py
@@ -1,4 +1,20 @@
-# This is Day 6
-with open('data.txt', 'r') as file:
-    data = file.read()
+''' This is Day *** '''
 
+
+
+YEAR = 0
+DAY = 0
+with open(f'AoC/AdventOfCode/AdventOfCode/AoC_{YEAR}/{YEAR}_Day_{DAY}/data.txt', 'r', encoding="utf-8") as file:
+    data = file.read().strip() #read file, and strip -- remove any white space
+
+with open(f'AoC/AdventOfCode/AdventOfCode/AoC_{YEAR}/{YEAR}_Day_{DAY}/test_data.txt', 'r', encoding="utf-8") as file:
+    test_data = file.read().strip()  # read file, and strip -- remove any white space    
+
+
+
+def part1(data):
+    """Function Solves problem one."""
+
+
+def part2(data):
+    """Function Solves problem two."""
