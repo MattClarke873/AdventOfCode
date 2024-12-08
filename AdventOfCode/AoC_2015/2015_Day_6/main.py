df57649a03d1b63980241b0ae1368e2348c4f934
MattClarke873 <matclarke216@hotmail.com>
2024-12-08 05:08:46 +0000

    start day 6

--- AdventOfCode/AoC_2015/2015_Day_6/main.py
+++ AdventOfCode/AoC_2015/2015_Day_6/main.py
@@ -1,20 +1,34 @@
-''' This is Day *** '''
+''' This is Day 6 '''
 
 
 
-YEAR = 0
-DAY = 0
-with open(f'AoC/AdventOfCode/AdventOfCode/AoC_{YEAR}/{YEAR}_Day_{DAY}/data.txt', 'r', encoding="utf-8") as file:
+YEAR = 2015
+DAY = 6
+with open(f'AoC/AdventOfCode/AdventOfCode/AoC_{YEAR}/{YEAR}_Day_{DAY}/data.txt',
+          'r',
+          encoding="utf-8") as file:
     data = file.read().strip() #read file, and strip -- remove any white space
 
-with open(f'AoC/AdventOfCode/AdventOfCode/AoC_{YEAR}/{YEAR}_Day_{DAY}/test_data.txt', 'r', encoding="utf-8") as file:
-    test_data = file.read().strip()  # read file, and strip -- remove any white space    
+with open(f'AoC/AdventOfCode/AdventOfCode/AoC_{YEAR}/{YEAR}_Day_{DAY}/test_data.txt',
+          'r',
+          encoding="utf-8") as file:
+    test_data = file.read().strip()  # read file, and strip -- remove any white space
 
 
 
-def part1(data):
+def part1():
     """Function Solves problem one."""
 
 
-def part2(data):
-    """Function Solves problem two."""
+# if turn on = 1
+# if turn off = 0
+# toggle = if 1 then = 0 and if 0 then = 1
+
+    print(data)
+
+
+def part2():
+    '''Function Solves problem two.'''
+
+
+part1()
